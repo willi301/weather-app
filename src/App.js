f2a0760edc4c8a31b7d165e31f3de0316ecdccd2
willi301 <will301will@gmail.comm>
2024-09-11 22:01:45 -0700

    add temperature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+
+
+
 
 function App() {
+
+  const [temperature, setTemperature] = useState("0");
+  
   return (
     <div className="App">
       <h1>SF Weather</h1>
       <h3>Temperature</h3>
-      <h2>0</h2>
+      <h2 id='temp'> {temperature} </h2>
       <h3>random comment in future</h3>
 
     </div>
