4184110c4d83504ce586932a9347057962ad406a
willi301 <will301will@gmail.comm>
2024-09-11 21:52:12 -0700

    add ui to main app

--- src/App.js
+++ src/App.js
@@ -4,20 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>SF Weather</h1>
+      <h3>Temperature</h3>
+      <h2>0</h2>
+      <h3>random comment in future</h3>
+
     </div>
   );
 }
